ba90bf13e0d4ae54de731526c90383e2e9114d74
udaymathur14@gmail.com <udaymathur14@gmail.com>
2023-04-04 11:33:41 +0530

    Added Entity framework and created database and seed data

---
+++ HotelListing.API/Data/Hotel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelListing.API.Data
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public double Rating { get; set; }
+
+
+        //every hotel belongs to a country , this is the foriegn key 
+        [ForeignKey(nameof(CountryId))]
+        public int CountryId { get; set; }  
+
+        public Country Country { get; set; }
+
+    }
+}
